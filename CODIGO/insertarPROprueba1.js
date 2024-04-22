8470abaf12ae2f941874dc0d539c08b1ab2233b4
ChechuNeta <127496996+ChechuNeta@users.noreply.github.com>
2024-04-22 21:06:04 +0200

    añadirprofemeh

---
+++ CODIGO/insertarPROprueba1.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('loginform').addEventListener('submit', function (event) {
+        event.preventDefault();
+        
+    });
+});
--- CODIGO/insertarPROprueba1.php
+++ CODIGO/insertarPROprueba1.php
@@ -6,11 +6,16 @@
             printf("<p>Conexión fallida: %s</p>", mysqli_connect_error());
             exit();}
 
+        $id = $numero_aleatorio = rand(10000, 99999);
         $nombre = $_GET['nombre'];
         $correo = $_GET['correo'];
         $contrasena = $_GET['pass'];
         $departamento = $_GET['dept'];
 
-        $profesor = mysqli_query($conexion, "INSERT INTO Profesor (ID_Profe, nombre, correo, clave_acceso, dep) VALUES (".(FLOOR(RAND() * 90000) + 10000).", '".$nombre."', '".$correo."', '".$contrasena."', 1)");
-        $NI = mysqli_num_rows($incidencias);
+        $profesor = mysqli_query($conexion, "INSERT INTO Profesor (ID_Profe, nombre, correo, clave_acceso, dep) VALUES (".$id.", '".$nombre."', '".$correo."', '".$contrasena."', ".$departamento.")");
+        
+        if (mysqli_connect_errno()) {
+            printf("<p>Conexión fallida: %s</p>", mysqli_connect_error());
+            exit();
+        }
  ?>
