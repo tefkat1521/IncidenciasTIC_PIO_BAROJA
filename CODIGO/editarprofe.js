4f4eb16c6a13626ebc1dbecdff753e37f2e536e2
VictoriaDorado <127496937+VictoriaDorado@users.noreply.github.com>
2024-05-18 16:52:04 +0200

    + editar profe empezado

---
+++ CODIGO/editarprofe.js
@@ -0,0 +1,41 @@
+$(document).ready(function() {
+    $(".login-box").fadeIn(1500, function () {
+        $(this).css("display", "block");
+    });
+
+    $('#loginForm').submit(function (event) {
+        event.preventDefault();
+        comprobarCredenciales(); // Llamada a la función para comprobar credenciales
+    });
+
+    $('#pswdButton').click(function(e) {
+        e.preventDefault(); // Evita que el formulario se envíe automáticamente
+        
+        var password1 = $('#password').val(); // Obtener el valor del primer campo de contraseña
+        var password2 = $('#confirmPassword').val(); // Obtener el valor del segundo campo de contraseña
+    
+        if (password1 !== password2) {
+            alert("Las contraseñas no coinciden. Por favor, inténtelo de nuevo.");
+            // Limpiar los campos de contraseña
+            $('#password').val('');
+            $('#confirmPassword').val('');
+            return false; // Detener el envío del formulario
+        } else {
+            // Mostrar el popup de confirmación
+            var confirmacion = confirm("¿Seguro que desea cambiar la contraseña?");
+            if (confirmacion) {
+                // Mostrar mensaje de contraseña cambiada correctamente
+                alert("Contraseña cambiada correctamente");
+                // Redirigir a la página index.html después de 2 segundos
+                // setTimeout(function() {
+                    window.location.href = "index.html";
+                // }, 1000);
+            } else {
+                // Si el usuario cancela, no hacer nada
+                return false;
+            }
+        }
+    });
+    
+    
+});
