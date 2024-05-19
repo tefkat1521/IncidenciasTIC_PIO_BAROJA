7cb34a812931bbac0b01b895d9618076a4179b57
ChechuNeta <127496996+ChechuNeta@users.noreply.github.com>
2024-05-19 21:14:36 +0200

    Revert "F"
    This reverts commit 7fe6e785be4c686d38a7984533e0a5fc166368c7.

--- CODIGO/admin.php
+++ CODIGO/admin.php
@@ -122,31 +122,65 @@ if (count($array_incidencias) > 0) {
                         <input type="hidden" name="id" value="1"><br>
                         <input type="submit" name="submit" value="Actualizar">
                     </form>
-                </div> <br>';
-                
-            }
-            
-            $html_output .= '
-                <div id="borrado">
-                    <form method="post" action="admin.php">
-                        <input type="hidden" name="id2" value="'.$laincidencia["id_incidencia"].'"?>
-                        <input type=submit name="submitborrado" value="Borrar">
-                    </form>
                 </div>
-            
-            </div>';
+            ';
         }
-    
-        $html_output .= '</div>';
 
+        $html_output .= '
+            <div id="borrado">
+                <form method="post" action="admin.php">
+                    <input type="hidden" name="id2" value="' . $laincidencia["id_incidencia"] . '">
+                    <input type="submit" name="submitborrado" value="Borrar">
+                </form>
+            </div>
+        </div>';
     }
-    else
-    {
-        $html_output .= '<div>No existen incidencias</div>';
-    }
-    
-    // Devuelve la salida HTML
-    echo $html_output;
+
+    $html_output .= '</div>';
+} else {
+    $html_output .= '<div>No existen incidencias</div>';
 }
+
+// Devuelve la salida HTML
+echo $html_output;
+
+}
+
+
+function procesarFormulario() {
+    if(isset($_POST["hecho"])) { 
+        $prof = new profesor();
+        $prof->insertar_profesor($_POST["nombre"], $_POST["correo"], $_POST["pass"], $_POST["dept"]);
+        if($prof) {
+            echo "Insertado profesor: " . $_POST["nombre"];
+            echo " Redirigiendo...";
+            header("Location: admin.html"); // Redirige a admin.html
+            exit; // Asegura que el script se detenga después de la redirección
+        }
+    }
+}
+function generarFormulario($array_deps) {
+    $out = '';
+    $out .= '<form id="loginform" action="admin.php" method="post">';
+    $out .= '<label>Nombre Completo</label>';
+    $out .= '<input type="text" name="nombre" required/><br>';
+    $out .= '<label>Correo</label>';
+    $out .= '<input type="text" name="correo" required/><br>';
+    $out .= '<label>Departamento</label>';
+    $out .= '<select name="dept" required>';
+    foreach($array_deps as $depart) {
+        $out .= "<option value='".$depart['dep']."'>".$depart['Nombre_dep']."</option>";
+    }
+    $out .= '</select><br><br>';
+    $out .= '<label>Contraseña</label>';
+    $out .= '<input type="password" name="pass" required/><br>';
+    $out .= '<label>Repita la contraseña</label>';
+    $out .= '<input type="password" required/><br>';
+    $out .= '<input type="submit" name="hecho" value="CREAR">';
+    $out .= '</form>';
+    echo $out;
+}
+
+
 
 ?>
