1b5e6611185c86008e6b27321a570f2752ac6960
VictoriaDorado <127496937+VictoriaDorado@users.noreply.github.com>
2024-05-30 21:17:46 +0200

    admin.js actualizado

--- CODIGO/admin.js
+++ CODIGO/admin.js
@@ -1,94 +1,7 @@
-// function consultarIncidencias() {
-//     var PaginaAdmin = 0;
-
-//     var xhttp = new XMLHttpRequest();
-
-//     xhttp.onreadystatechange = function () {
-//         if (this.readyState == 4 && this.status == 200) {
-//             var TablaIncidencias = this.response; // Obtener la respuesta del servidor
-//             var DivIncidencias = document.getElementById("incidenciasAdmin");
-
-//             DivIncidencias.innerHTML = TablaIncidencias;
-
-//             // Añadir manejadores de eventos después de que el contenido se haya cargado
-//             $(document).ready(function() {
-//                 $('[id^=toggle-pencil]').click(function() {
-//                     var index = this.id.split('-')[2];
-//                     var form1 = $('#form1-' + index);
-//                     form1.css('visibility', form1.css('visibility') === 'hidden' ? 'visible' : 'hidden');
-//                 });
-
-//                 $('[id^=toggle-sort]').click(function() {
-//                     var index = this.id.split('-')[2];
-//                     var form2 = $('#form2-' + index);
-//                     form2.css('visibility', form2.css('visibility') === 'hidden' ? 'visible' : 'hidden');
-//                 });
-//             });
-//         }
-//     };
-
-//     xhttp.open("POST", "admin.php", true); // true indica una solicitud asíncrona
-//     xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-//     xhttp.send("PaginaAdmin=" + PaginaAdmin);
-// }
-
-
-// function consultarIncidencias() {
-//     var value = 0; // Inicializar value a 0
-
-//     $('.filtro_incidencia').on('click', function () {
-//         // Eliminar la clase 'active' de todos los botones
-//         $('.filtro_incidencia').removeClass('active');
-
-//         // Añadir la clase 'active' al botón pulsado
-//         $(this).addClass('active');
-
-//         // Obtener el valor del botón pulsado
-//         value = $(this).data('value');
-//         console.log(value);
-
-//         // Realizar la solicitud AJAX
-//         $.ajax({
-//             url: 'admin.php',
-//             type: 'POST',
-//             data: { value: value },
-//             success: function (response) {
-//                 // Actualizar el contenido del div con la respuesta
-//                 $('#result').html(response);
-
-//                 // Añadir manejadores de eventos después de que el contenido se haya cargado
-//                 $('[id^=toggle-pencil]').click(function () {
-//                     var index = this.id.split('-')[2];
-//                     var form1 = $('#form1-' + index);
-//                     form1.css('display', form1.css('display') === 'none' ? 'block' : 'none');
-//                 });
-
-//                 $('[id^=toggle-sort]').click(function () {
-//                     var index = this.id.split('-')[2];
-//                     var form2 = $('#form2-' + index);
-//                     form2.css('display', form2.css('display') === 'none' ? 'block' : 'none');
-//                 });
-
-//             }
-//         });
-//     });
-// }
-
-
 function consultarIncidencias() {
     var value = 0; // Inicializar value a 0
 
-    $('.filtro_incidencia').on('click', function () {
-        // Eliminar la clase 'active' de todos los botones
-        $('.filtro_incidencia').removeClass('active');
-
-        // Añadir la clase 'active' al botón pulsado
-        $(this).addClass('active');
-
-        // Obtener el valor del botón pulsado
-        value = $(this).data('value');
-        // console.log(value);
-
+    function realizarSolicitud() {
         // Realizar la solicitud AJAX
         $.ajax({
             url: 'admin.php',
@@ -108,9 +21,29 @@ function consultarIncidencias() {
                     var index = this.id.split('-')[2];
                     toggleForms(index, 'form2');
                 });
-
+            },
+            error: function() {
+                alert('Error al procesar la solicitud. Por favor, inténtalo de nuevo.');
             }
         });
+    }
+
+    // Llamar a realizarSolicitud al cargar la página para enviar el valor inicial 0
+    realizarSolicitud();
+
+    $('.filtro_incidencia').on('click', function () {
+        // Eliminar la clase 'active' de todos los botones
+        $('.filtro_incidencia').removeClass('active');
+
+        // Añadir la clase 'active' al botón pulsado
+        $(this).addClass('active');
+
+        // Obtener el valor del botón pulsado
+        value = $(this).data('value');
+        console.log(value);
+
+        // Realizar la solicitud AJAX
+        realizarSolicitud();
     });
 }
 
@@ -134,20 +67,13 @@ function toggleForms(index, formType) {
     }
 }
 
-
-
-
-
 function insertarProfesor() {
     var xhttp = new XMLHttpRequest();
     var newProfesor = 0;
     xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
-            
-            // Aquí puedes hacer algo con la respuesta del servidor, si es necesario
             var insertarProfesor = this.response; // Obtener la respuesta del servidor
             var DivProfesor = document.getElementById("insertarProfesor");
-
             DivProfesor.innerHTML = insertarProfesor;
         }
     };
@@ -155,8 +81,6 @@ function insertarProfesor() {
     xhttp.open("POST", "admin.php", true);
     xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
     xhttp.send("newProfesor=" + newProfesor);
-
-    // xhttp.send("nombre=" + nombre + "&correo=" + correo + "&pass=" + pass + "&dept=" + dept + "&newProfesor=1");
 }
 
 function SesionUser() {
@@ -184,12 +108,7 @@ function noLogueado() {
     window.location.href = "login.html";
 }
 
-
-
-function editarIncidencia(numIncidencia){
-    // prueba = this.innerHTML;
-    // console.log(prueba);
-
+function editarIncidencia(numIncidencia) {
     $.ajax({
         type: 'POST',
         url: 'admin.php',
@@ -200,13 +119,10 @@ function editarIncidencia(numIncidencia){
             console.log(response);
         },
         error: function () {
-            // Si hay un error en la solicitud AJAX, mostrar un mensaje de alerta
             alert('Error al procesar la solicitud. Por favor, inténtalo de nuevo.');
         }
     });
 }
-
-
 
 document.addEventListener("DOMContentLoaded", function () {
     consultarIncidencias();
@@ -214,19 +130,9 @@ document.addEventListener("DOMContentLoaded", function () {
     insertarProfesor();
 });
 
-
-
-// NO FUNCIONA
-
-
 document.addEventListener("DOMContentLoaded", function () {
-    
-
-
-        var incidenciasAdmin = document.getElementById('incidenciasAdmin');
-        var primerDivHijo = incidenciasAdmin.querySelector('div');
-  
-
+    var incidenciasAdmin = document.getElementById('incidenciasAdmin');
+    var primerDivHijo = incidenciasAdmin.querySelector('div');
 
     const incidencias = document.querySelectorAll("#incidencias > div");
     const grande = document.getElementById("grande");
@@ -244,4 +150,3 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     });
 });
-
