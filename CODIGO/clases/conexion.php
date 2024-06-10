36c2fbbf2b7438126c0df25e96a3bc86495dc5f1
tefkat1521 <84088372+tefkat1521@users.noreply.github.com>
2024-06-10 14:22:43 +0200

    y

--- CODIGO/clases/conexion.php
+++ CODIGO/clases/conexion.php
@@ -6,7 +6,7 @@ class Conexion
 
     public function __construct()
     {
-        $this->conect = new mysqli("localhost","admin_ftp",'admin',"incidencias_tic");
+        $this->conect = new mysqli("127.0.0.1", "admindb", "admin", "incidencias_tic");
         if($this->conect->connect_errno) {
             echo "Fallo al conectar con la BBDD MySql: ". $this->conect->connect_errno;
             return;
